73faa0dcf404bf3906f9da866d5aa28e952e6d0f
Roman <85108549+Roman-Vik@users.noreply.github.com>
2024-04-20 15:58:00 +0300

    Update mymath.go

--- mymath.go
+++ mymath.go
@@ -4,6 +4,6 @@ package mymath
 import "math"
 
 // Sqrt возвращает квадратный корень из числа x.
-func Sqrt(x float64) float64 {
-    return math.Sqrt(x)
+func Sqrt(x float64) int {
+    return int(math.Sqrt(x))
 }
